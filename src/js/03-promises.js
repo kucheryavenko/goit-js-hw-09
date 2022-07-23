d0b7f6facb61f1e11648c02e20ffb10ea32660bc
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2022-07-23 13:55:59 +0300

    task-03-done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,59 +1,42 @@
-// import { Notify } from 'notiflix/build/notiflix-notify-aio'; // Импорт библиотеки "notify"
+import { Notify } from 'notiflix/build/notiflix-notify-aio'; // Импорт библиотеки "notify"
 
-// const formEl = document.querySelector('.form');
-// const delayEl = document.querySelector('input[name="delay"]');
-// const stepEl = document.querySelector('input[name="step"]');
-// const amountEl = document.querySelector('input[name="amount"]');
-
-// formEl.addEventListener('submit', onSubmit);
-
-// function onSubmit(evt) {
-//     evt.preventDefault();
-
-//     const step = Number(stepEl.value);
-//     let delay = Number(delayEl.value);
-//     let amountInterval = 0;
-    
-//     const intervalID = setInterval(() => {
-//         amountInterval += 1;
-//         if (amountEl.value < amountInterval) {
-//             clearInterval(intervalID);
-//             return;
-//         }
-//         createPromise(amountInterval, delay).then(onFulfilled).catch(onRejected);
-//         delay += step;
-//     }, delay);
-// }
-
-// function createPromise(position, delay) {
-//     const shouldResolve = Math.random() > 0.3;
-    
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             if (shouldResolve) {
-//                 resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//             } else {
-//                 reject(`❌ Rejected promise ${position} in ${delay}ms`);
-//             }
-//         }, delay);
-//     });
-// }
-
-// function onFulfilled(result) {
-//     Notify.success(result);
-// }
-// function onRejected(error) {
-//     Notify.failure(error);
-// }
-
-/** */
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
+// Находим наши элементы и создаём переменные
 const formEl = document.querySelector('.form');
 const delayEl = document.querySelector('input[name="delay"]');
 const stepEl = document.querySelector('input[name="step"]');
 const amountEl = document.querySelector('input[name="amount"]');
 
+// Вешаем слушателя на форму и по сабмиту запускаем ф-цию
+formEl.addEventListener('submit', onSubmit);
+
+// Ф-ция собирает промисы с интервалом
+function onSubmit(evt) {
+    evt.preventDefault();
+
+    const stepValue = Number(stepEl.value);
+    const amountValue = Number(amountEl.value);
+    let delayValue = Number(delayEl.value);
+    let amountInterval = 0;
+
+    const intervalId = setInterval(() => {
+
+        amountInterval += 1;
+
+        if (amountValue < amountInterval) {
+            clearInterval(intervalId);
+            return;
+        }
+
+        createPromise(amountInterval, delayValue)
+            .then(onMakeFulfilled)
+            .catch(onMakeRejected);
+        
+        delayValue += stepValue;
+
+    }, delayValue)
+}
+
+// Ф-ция создает и возвращает промисы
 function createPromise(position, delay) {
     const shouldResolve = Math.random() > 0.3;
     
@@ -64,32 +47,22 @@ function createPromise(position, delay) {
             } else {
                 reject(`❌ Rejected promise ${position} in ${delay}ms`);
             }
-        }, 1000);
+        }, delay);
     }); 
 }
 
+// Ф-ция выводит результат resolve
 function onMakeFulfilled(result) {
     console.log(result);
     Notify.success(result, {
-    useIcon: false,
+        useIcon: false,
     });
 }
 
-function onMakeRejected(result) {
-    console.log(result);
-    Notify.failure(result, {
-    useIcon: false,
+// Ф-ция выводит результат reject
+function onMakeRejected(error) {
+    console.log(error);
+    Notify.failure(error, {
+        useIcon: false,
     });
 }
-
-createPromise(2, 1500)
-  .then(onMakeFulfilled)
-  .catch(onMakeRejected);
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
